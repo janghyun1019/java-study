c71d48d89acbe56b5722bc11246ae4ef40be095f
janghyun1019 <mon.p.a.hyun@gmail.com>
2024-10-23 08:59:08 +0900

    아침 혼공

---
+++ src/study/alone/IntegerListeralExample.java
@@ -0,0 +1,20 @@
+package study.alone;
+
+public class IntegerListeralExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int var1 = 0b1011;
+		int var2 = 0206;
+		int var3 = 365;
+		int var4 = 0xB3;
+		
+		System.out.println("var1: " + var1);
+		System.out.println("var1: " + var2);
+		System.out.println("var1: " + var3);
+		System.out.println("var1: " + var4);
+
+	}
+
+}
