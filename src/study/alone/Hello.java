1bbc0f88203640e479813eff87407148876df16a
janghyun1019 <mon.p.a.hyun@gmail.com>
2024-10-22 09:01:38 +0900

    혼자복습시작

---
+++ src/study/alone/Hello.java
@@ -0,0 +1,15 @@
+package study.alone;
+
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 장제목: 1장 자바 복습하기
+		 작성일 2024.10.22
+		 * */
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, java");
+	}
+
+}
