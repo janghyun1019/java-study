fc1008ee234cde234b138b19106b98fd31f34274
janghyun1019 <mon.p.a.hyun@gmail.com>
2024-10-25 16:53:40 +0900

    메뉴고르기 로직 연습

---
+++ src/study/practice/Parc15_241025.java
@@ -0,0 +1,182 @@
+package study.practice;
+
+import java.util.Scanner;
+
+public class Parc15_241025 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		//배열 활용 버전
+		
+				Scanner scanner = new Scanner(System.in);
+				int menu;	//몇번 메뉴 입력
+				int count;  //몇개 주문 
+				String addOrder; // y/n 
+				
+				String[] menuArr = {"아메리카노", "카페라떼", "바닐라라떼", "초코라떼"}; //메뉴이름
+				int[] priceArr = {3500, 4100, 4500, 5000}; //메뉴가격
+				//		index :    0    1     2
+				int[] countArr = new int[menuArr.length];  //메뉴 주문 수량
+				//		index : 0 1 2 
+				// 사용자선택    : 1 2 3 
+				//index0:아메리카노 수량
+				//index1:카페라떼 수량
+				//index2:바닐라라떼 수량
+				
+				while (true) {
+					//아메리카노 3800
+					
+					while(true) {
+						System.out.println("=========메뉴=========");
+						for(int i=0; i<menuArr.length; i++) {	//0~
+							// 1.메뉴이름 메뉴가격원
+							// 2.메뉴이름 메뉴가격원
+							// 3.메뉴이름 메뉴가격원
+							System.out.printf("%d.%-8s %6d원\n", i+1, menuArr[i], priceArr[i] );
+						}
+						System.out.println("=====================");
+			
+						System.out.print("메뉴 선택 : "); // 1, 2, 3
+						menu = scanner.nextInt();
+						
+						if( menu >= 1 && menu <= menuArr.length ) {
+							break;
+						} else {
+							System.out.println("잘못입력했습니다. 다시 입력하세요");
+						}
+					}
+					
+					//메뉴 입력 완료
+					//수량 입력받기
+					System.out.print("수량 선택 : ");
+					count = scanner.nextInt();  //몇개
+					
+					countArr[menu-1] += count; //수량 증가시키기
+					
+					//추가주문 여부
+					scanner.nextLine();
+					while(true) {
+						System.out.print("추가 주문하시겠습니까?(y/n) : ");
+						addOrder = scanner.nextLine(); // 문자열
+						
+						if(addOrder.equals("y") || addOrder.equals("n"))
+							break;
+						else 
+							System.out.println("잘못입력했습니다. 다시 입력하세요");
+					}
+					
+					if(addOrder.equals("n")) {
+						//입력받는 반복문 끝.
+						//주문한 메뉴 보여주고
+						//아메 몇개 * 가격
+						int total = 0;
+						System.out.println("=====================");
+						for(int i=0; i<menuArr.length; i++) {
+							
+							if(countArr[i] > 0) {
+								System.out.printf("%s %d잔 : %d원\n", menuArr[i], countArr[i], countArr[i]*priceArr[i]);
+								total = total + (countArr[i]*priceArr[i]);
+							}
+						}
+						
+						//total 총액 저장
+						System.out.println("=====================");
+						System.out.println("총액 : " + total + "원");
+						break;
+					}
+
+				}
+				
+				
+				
+				
+				//기본타입 변수들, 각각 처리 버전
+				/*
+				
+				Scanner scanner = new Scanner(System.in);
+				int menu;
+				int count;
+				String addOrder;
+				
+				int menu1Count = 0;
+				int menu2Count = 0;
+				int menu3Count = 0;
+				
+				while (true) {
+					
+					System.out.println("=========메뉴=========");
+					System.out.printf("%-10s%8s\n", "1.아메리카노", "3500원");
+					System.out.printf("%-10s%8s\n", "2.카페라떼", "4100원");
+					System.out.printf("%-10s%8s\n", "3.바닐라라떼", "4300원");
+					System.out.println("=====================");
+
+					System.out.print("메뉴 선택 : "); // 1, 2, 3
+					menu = scanner.nextInt();
+					
+					if (menu == 1 || menu == 2 || menu == 3) { // 정상선택
+
+						System.out.print("수량 선택 : ");
+						count = scanner.nextInt();	
+						
+						if(menu == 1)
+							menu1Count = menu1Count + count;  
+						
+						if(menu == 2)
+							menu2Count += count;
+						
+						if(menu == 3)
+							menu3Count += count;
+						
+						scanner.nextLine();// 입력값 청소
+						
+						while(true) {
+							System.out.print("추가 주문하시겠습니까?(y/n) : ");
+							addOrder = scanner.nextLine(); // 문자열
+							
+							if(addOrder.equals("y") || addOrder.equals("n"))
+								break;
+							else 
+								System.out.println("잘못입력했습니다. 다시 입력하세요");
+						}
+						//잘못된입력, 정상입력
+						
+						//y 
+						if(addOrder.equals("n")) {
+							//입력받는 반복문 끝.
+							//주문한 메뉴 보여주고
+							//아메 몇개 * 가격
+							if(menu1Count > 0)
+								System.out.printf("아메리카노 %d잔 : %d원\n", menu1Count, menu1Count*3500);
+							
+							//라뗴 몇개 * 가격
+							if(menu2Count > 0)
+								System.out.printf("카페라떼 %d잔 : %d원\n", menu2Count, menu2Count*4100);
+							
+							//바닐라 몇개 * 가격
+							if(menu3Count > 0)
+								System.out.printf("바닐라라떼 %d잔 : %d원\n", menu3Count, menu3Count*4300);
+							//누적합 계산 -> 총액 -> 금액 보여주기 -> 끝.
+							
+							int total = menu1Count*3500 +  menu2Count*4100 + menu3Count*4300;
+							System.out.println("총액 : " + total + "원");
+							break;
+						}
+
+					} else { // 메뉴 보여주고, 메뉴 입력을 다시 받기
+						System.out.println("잘못입력하셨습니다. 다시 입력하세요.");
+					}
+				}
+				*/
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- src/study/practice/Prac14_241024.java
+++ src/study/practice/Prac14_241024.java
@@ -8,39 +8,86 @@ public class Prac14_241024 {
 		// TODO Auto-generated method stub
 		
 	
-			System.out.println("1.입력");
-			System.out.println("2.수정");
-			System.out.println("3.조회");
-			System.out.println("4.삭제");
-			System.out.println("5.종료");
-			System.out.print("메뉴번호를입력하세요 : ");
+		Scanner scanner = new Scanner(System.in);
+		int menu = 0;
+		
+		String[] menuArr = {"입력", "수정", "조회", "삭제", "종료"};
+		
+		//if ==
+		//switch
+		//array 배열
+		
+		while(true) {
+		
+			for(int i=0; i<menuArr.length; i++) {
+				//i: 0 1 2 3 4
+				//   1 2 3 4 5
+				//menuArr[i] : 값에 접근
+				System.out.printf("%d. %s\n", (i+1), menuArr[i]);
+			}
+			System.out.print("메뉴 번호를 입력하세요 :");			
+			menu = scanner.nextInt();
 			
-			Scanner scanner = new Scanner(System.in); 
-			int menu=scanner.nextInt();
+			if(menu >=1 && menu <= 4) {
+				System.out.println( menuArr[menu-1] + " 메뉴입니다." );
+			} else if (menu == 5) {
+				System.out.println("프로그램이 종료됩니다.");
+				break;
+			} else {
+				System.out.println("잘못입력하셨습니다.");
+			}
+			System.out.println();
 			
-
-			if(menu >=1 && menu<= 5) { // 정상범위
-				// menu 1~5
+			
+			/*
+			System.out.println("1. 입력");
+			System.out.println("2. 수정");
+			System.out.println("3. 조회");
+			System.out.println("4. 삭제");
+			System.out.println("5. 종료");
+			System.out.print("메뉴 번호를 입력하세요 :");			
+			
+			menu = scanner.nextInt();
+			
+			if(menu == 1) {
+				System.out.println("입력 메뉴입니다.");
+			} else if (menu ==2 ) {
+				System.out.println("수정 메뉴입니다.");
+			} else if (menu == 3 ) {
+				System.out.println("조회 메뉴입니다.");
+			} else if (menu == 4 ) {
+				System.out.println("삭제 메뉴입니다.");
+			} else if (menu ==5){ //5
+				System.out.println("프로그램이 종료됩니다.");
+				break;
+			} else {
+				System.out.println("잘못입력하셨습니다.");
+			}*/
+			
+			
+			/*
+			if(menu >= 1 && menu <= 5) { //정상 메뉴 선택
+				//menu 1~5
 				
 				if(menu == 1) {
-					System.out.println("입력메뉴입니다.");
-				} else if (menu == 2) {
-					System.out.println("수정메뉴입니다.");
-				} else if (menu == 3) {
-					System.out.println("조회메뉴입니다.");
-				} else if (menu == 4) {
-					System.out.println("삭제메뉴입니다.");
+					System.out.println("입력 메뉴입니다.");
+				} else if (menu ==2 ) {
+					System.out.println("수정 메뉴입니다.");
+				} else if (menu == 3 ) {
+					System.out.println("조회 메뉴입니다.");
+				} else if (menu == 4 ) {
+					System.out.println("삭제 메뉴입니다.");
 				} else { //5
 					System.out.println("프로그램이 종료됩니다.");
-//					return;
-				} 
+					break;
+				}
 				
-			} else { // 잘못됨
+			} else { //잘못됨 
 				System.out.println("잘못입력하셨습니다.");
-				
 			}
+			*/
 			
-			System.out.println();
+		}
 			
 		
 
