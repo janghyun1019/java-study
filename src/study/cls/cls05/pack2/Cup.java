cb7e0c9529f92bd3307429213b139663e399ae84
janghyun1019 <mon.p.a.hyun@gmail.com>
2024-12-17 16:46:36 +0900

    정적멤버, 패키지, 접근제한자 -2

---
+++ src/study/cls/cls05/pack2/Cup.java
@@ -0,0 +1,14 @@
+package study.cls.cls05.pack2;
+
+public class Cup {
+	
+	public String type;  //Public은 외부에서 사용가능하게 함
+	// String type; //아무것도 쓰지 않으면 디폴트 비워두면 디폴트
+//	 public protected default("비워두면..") private
+	
+	
+	public void method() {
+		System.out.println("pack2.Cup method() 222");
+	}
+
+}
