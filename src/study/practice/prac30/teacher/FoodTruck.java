cc41592d553dcefe1989b72b0fa11c891b3757df
janghyun1019 <mon.p.a.hyun@gmail.com>
2024-12-30 11:37:27 +0900

    푸드트럭 1차 완성

--- src/study/practice/prac30/teacher/FoodTruck.java
+++ src/study/practice/prac30/teacher/FoodTruck.java
@@ -57,6 +57,8 @@ public class FoodTruck {
 //		        // TODO: handle exception
 //		    }
 		    
+		    int orderMenu;
+		    
 		    switch(menu) {
 		    case 1:  // 메뉴보기
 		    	//List
@@ -74,12 +76,64 @@ public class FoodTruck {
 		    		System.out.printf("[%d] %s\n", i+1, foodList.get(i).getFoodInfo());
 		    	}
 		    	System.out.println(">>주문 메뉴를 선택하세요 :");
-		    	int orderMenu = scanner.nextInt();
+		    	orderMenu = scanner.nextInt();
+		    	
+		    	//주문했다~ 재고처리
+		    	
+		    	
+		    	//예외처리 -> 숫자범위를 벗어났나?, 잘못된 값이 입력 되었나?
+		    	
+		    	
+		    	//조건 : 재고가 없으면 주문 불가 -> 해당 메뉴의 재고를 확인
+		    	if(foodList.get(orderMenu-1).stock > 0) {
+		    		//재고가 있으면? 주문 1개 처리
+		    		//주문완료 정보 출력
+		    		System.out.printf("[주문정보] %s 메뉴 1개 주문완료!\n",foodList.get(orderMenu-1).name);
+		    		
+		    		//재고가 하나 깍여야함
+		    		//재고 -1개 처리
+//		    		foodList.get(orderMenu-1).stock -+ 1;
+		    		foodList.get(orderMenu-1).modifyStock(-1);
+		    		
+		    		//몇개 주문하시겠어요? 주문 수량 입력 :
+		    		
+		    		//매출액 증가
+		    		totalSales += foodList.get(orderMenu-1).price;
+		    		
+		    	} else {
+		    		System.out.println("[품절] 해당메뉴는 품절입니다.");
+		    	}	    	
+		    	
+		    	
 		    	break;
 		    case 3:  // 재고관리
-		    break;
+		    	for(int i=0; i<foodList.size(); i++) {
+		    		System.out.printf("[%d] %s\n", i+1, foodList.get(i).getFoodInfo());
+		    	} // 메뉴 보여주기
+		    	System.out.println(">>주문 메뉴를 선택하세요 :");
+		    	orderMenu = scanner.nextInt();
+		    	
+		    	System.out.println(">>>재고 조정 개수를 입력하세요 : ");
+		    	int changeStockCount = scanner.nextInt(); //10  -5  30
+		    	
+		    	foodList.get(orderMenu-1).modifyStock(changeStockCount);  //수량조정
+		    	System.out.println("[재고 적용 완료");
+		    	System.out.println(foodList.get(orderMenu-1).getFoodInfo());
+		    			    	
+		    	
+		    	break;
 		    case 4:  //마감하기
 		    	// 매출 / 원가차감 -> 최종 수익?
+		    	int lossSales = 0;
+		    	for(Food food: foodList) {
+		    		lossSales += (int)(food.price * 0.3) * food.stock;
+		    	}
+		    	
+		    	System.out.println("[마감정보]");
+		    	System.out.println(">총 매출: " + totalSales);
+		    	System.out.println(">폐기손해: " + lossSales);
+		    	System.out.println(">최종수익: " + (totalSales - lossSales));
+		    	
 		    	isOpened = false;
 		    	System.out.println("===푸드트럭 영업마감===");
 		    	break;
